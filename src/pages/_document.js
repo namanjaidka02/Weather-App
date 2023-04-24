7bab7154b9b720f9eedbf06b9f04a63e8b12cf17
Naman <namanjaidka89@gmail.com>
2023-04-24 22:43:07 +0530

    weather-app

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <link
+          rel="stylesheet"
+          href="/fontawesome-free-5.15.3-web/css/all.min.css"
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
